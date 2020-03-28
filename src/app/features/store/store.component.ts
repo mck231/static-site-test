fad7e3174d494f5c8503a811010aea5e03673e00
Marco Cabrera <highlandsmac@yahoo.com>
2020-03-27 20:58:51 -0500

    first page

---
+++ src/app/features/store/store.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store',
+  templateUrl: './store.component.html',
+  styleUrls: ['./store.component.scss']
+})
+export class StoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+   //Check if there any unsaved data etc. If yes then as for confirmation
+ canExit() : boolean {
+
+  if (confirm("Do you wish to Please confirm")) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+}
