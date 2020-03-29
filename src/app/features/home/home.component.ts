358fcf250531e0be7c1ad4e10eb3a4fff2737134
Marco Cabrera <highlandsmac@yahoo.com>
2020-03-29 00:40:42 -0500

    added music to site

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Howl, Howler} from 'howler';
 
 export interface Tile {
   color: string;
@@ -16,10 +17,23 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let sound = new Howl({
+      src: ['./././assets/pennies.wav'],
+      autoplay: true,
+      loop: true,
+      volume: 0.5,
+      onend: function() {
+        console.log('Finished!');
+      }
+    });
   }
   tiles: Tile[] = [
     {text: '', cols: 3, rows: 2, color: 'lightgreen'},
     {text: '', cols: 1, rows: 2, color: ''}
+  ];
+  tiles2: Tile[] = [
+    {text: '', cols: 4, rows: 1, color: 'lightgrey'},
+    {text: 'A DJ?', cols: 1, rows: 2, color: ''}
   ];
 
 }
