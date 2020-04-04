1228afa7393a054166b72616fb7e2a306954dbda
Marco Cabrera <highlandsmac@yahoo.com>
2020-04-03 20:06:24 -0500

    update to mobile

--- src/app/features/audio-player/audio-player.component.ts
+++ src/app/features/audio-player/audio-player.component.ts
@@ -12,17 +12,5 @@ export class AudioPlayerComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  public playSong()
-  {
-
-    let sound = new Howl({
-      src: ['https://firebasestorage.googleapis.com/v0/b/who-is-marco-cabrera.appspot.com/o/pennies.wav?alt=media&token=755552d7-f0f0-49b0-b121-25a20b456a0c'],
-      autoplay: false,
-      loop: true,
-      volume: 0.5,
-      onend: function() {
-        console.log('Finished!');
-      }
-    });
-  }
+  
 }
